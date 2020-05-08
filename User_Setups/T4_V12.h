2fc1b2de5a287111ae469d0ad1a6d3831415d0f4
Pawat Saengduan <saengduanpawat@gmail.com>
2020-05-08 08:57:43 +0700

    A manually user setups board TTGO T4 V1.2

---
+++ User_Setups/T4_V12.h
@@ -0,0 +1,15 @@
+#define TFT_MISO 12
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS 27
+#define TFT_DC 26
+#define TFT_RST 5
+#define TFT_BL -1
+
+#define SD_MISO 13
+#define SD_MOSI 15
+#define SD_SCLK 2
+#define SD_CS 14
+
+#define CHANNEL_0 0
+#define BOARD_VRESION "<T4 V1.2>"
